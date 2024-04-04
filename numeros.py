1700a4793582ff022e0426f052c64b096974dee9
alcrm61 <belinhalecrim33@gmail.com>
2024-04-04 19:02:49 -0300

    Create numeros.py
    gilmar

---
+++ numeros.py
@@ -0,0 +1,34 @@
+# questionario com multiplas alternativas
+
+print("Bem-vindo ao questionário!")
+print("Responda as seguintes perguntas:")
+
+# Questão 1
+print("\n1. pergunta 01")
+print("a) alternativa01")
+print("b) alternativa02")
+print("c) alternativa03")
+print("d) alternativa04")
+
+resposta_1 = input("Sua resposta (a, b, c ou d): ").lower()
+
+# Verifica a resposta da Questão 1
+
+if resposta_1 == "b":
+    print("Correto!")
+else:
+    print("Incorreto. A capital da França é Paris.")
+
+# Questão 2 - Verdadeiro ou Falso
+
+print("\n1. pergunta 02")
+print("a) Verdadeiro")
+print("b) Falso")
+
+resposta_2 = input("Sua resposta (a ou b): ").lower()
+
+# Verifica a resposta da Questão 1
+if resposta_2 == "a":
+    print("Correto!")
+else:
+    print("Incorreto. a resposta será...")
